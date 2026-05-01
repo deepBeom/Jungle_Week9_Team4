37c817ff2d20548ad123acb0d3792abafc27d84f
kael <sskimhd34@gmail.com>
2026-05-01 19:43:51 +0900

    Feat: UScriptComponent 클래스 추가
    Changes to be committed:
    	new file:   NipsEngine/Source/Engine/Component/ScriptComponent.cpp
    	new file:   NipsEngine/Source/Engine/Component/ScriptComponent.h

---
+++ NipsEngine/Source/Engine/Component/ScriptComponent.cpp
@@ -0,0 +1,17 @@
+﻿#include "Core/EnginePCH.h"
+#include "ScriptComponent.h"
+
+void UScriptComponent::BeginPlay()
+{
+
+}
+
+void UScriptComponent::Tick(float DeltaTime)
+{
+
+}
+
+void UScriptComponent::HandleOverlap(AActor* OtherActor)
+{
+
+}
---
+++ NipsEngine/Source/Engine/Component/ScriptComponent.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Engine/Component/ActorComponent.h"
+#include "sol2/include/sol/sol.hpp"
+
+class UScriptComponent : public UActorComponent
+{
+public:
+    void BeginPlay() override;
+    void Tick(float DeltaTime);
+
+    void HandleOverlap(AActor* OtherActor);
+
+private:
+    std::string ScriptPath;
+
+    sol::environment Env;
+    sol::protected_function LuaOnBeginPlay;
+    sol::protected_function LuaOnTick;
+    sol::protected_function LuaOnOverlap;
+};
